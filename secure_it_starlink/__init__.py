3b62daffc325e2a75704fa8837f8fd1acde7a13d
Daniel Azevedo Novais <daniel.novais@sempreceub.com>
2026-01-17 20:26:47 -0300

    Refactor module imports and update module docstring for clarity

--- secure_it_starlink/__init__.py
+++ secure_it_starlink/__init__.py
@@ -1,4 +1,4 @@
-"""Secure IT Starlink.
+"""Secure IT Infrastructure - Starlink.
 
 Enterprise-grade security solutions for managed Starlink infrastructures.
 """
@@ -7,12 +7,13 @@ __version__ = "1.0.0"
 __author__ = "Secure IT Team"
 
 from .access import AccessController, AuthenticationManager
-from .automated_responses import AutomatedResponseCoordinator
-from .config import ConfigScanner, ConfigurationManager, SecurityConfig
+from .automated_responses.coordinator import AutomatedResponseCoordinator
+from .config import ConfigScanner, SecurityConfig
+from .config.config_loader import ConfigurationManager
 from .crypto import EncryptionManager, KeyManager
 from .logging import AlertManager, SecurityLogger
-from .logging.structured_logger import EventCorrelator, StructuredLogger
-from .metrics import MetricsCollector
+from .logging.structured_logger import StructuredLogger
+from .metrics.collector import MetricsCollector
 from .network import ConnectionValidator, NetworkMonitor
 from .scanning import PortScanner, VulnerabilityScanner
 
@@ -24,40 +25,13 @@ __all__ = [
     "ConfigScanner",
     "ConfigurationManager",
     "ConnectionValidator",
-    """Secure IT Infrastructure - Starlink.
-
-    Enterprise-grade security solutions for managed Starlink infrastructures.
-    """
-
-    __version__ = "1.0.0"
-    __author__ = "Secure IT Team"
-
-    from .access import AccessController, AuthenticationManager
-    from .automated_responses.coordinator import AutomatedResponseCoordinator
-    from .config import ConfigScanner, SecurityConfig
-    from .config.config_loader import ConfigurationManager
-    from .crypto import EncryptionManager, KeyManager
-    from .logging import AlertManager, SecurityLogger
-    from .logging.structured_logger import StructuredLogger
-    from .metrics.collector import MetricsCollector
-    from .network import ConnectionValidator, NetworkMonitor
-    from .scanning import PortScanner, VulnerabilityScanner
-
-    __all__ = [
-        "AccessController",
-        "AlertManager",
-        "AuthenticationManager",
-        "AutomatedResponseCoordinator",
-        "ConfigScanner",
-        "ConnectionValidator",
-        "ConfigurationManager",
-        "EncryptionManager",
-        "KeyManager",
-        "MetricsCollector",
-        "NetworkMonitor",
-        "PortScanner",
-        "SecurityConfig",
-        "SecurityLogger",
-        "StructuredLogger",
-        "VulnerabilityScanner",
-    ]
+    "EncryptionManager",
+    "KeyManager",
+    "MetricsCollector",
+    "NetworkMonitor",
+    "PortScanner",
+    "SecurityConfig",
+    "SecurityLogger",
+    "StructuredLogger",
+    "VulnerabilityScanner",
+]
